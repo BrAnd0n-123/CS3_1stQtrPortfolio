eeff58ae498665b34e56ed9ba61ebdb621f24d01
BrAnd0n-123 <b2029btpfernando@pshs.edu.ph>
2025-08-07 10:21:11 +0800

    finishingfirstngtask

---
+++ public/script.js
@@ -0,0 +1,14 @@
+let age =13;
+
+let maxAge = 100;
+
+let numPerDay = 1;
+
+let totalRequired;
+totalRequired = (numPerDay * 365) * (maxAge - age);
+
+const statement1 = "You will need ";
+const statement2 = "to last you until the ripe old age of ";
+const snack = " skyflakes ";
+
+document.write(statement1 + totalRequired + snack + statement2 + maxAge);
